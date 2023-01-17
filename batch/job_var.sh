00eb455b016d782a0f78f6a1d818f15eb1db79ef
sebo742e <sebo742e@tauruslogin6.taurus.hrsk.tu-dresden.de>
2023-01-17 18:57:27 +0100

    Added more shell scripts for parallel processing

---
+++ batch/job_var.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+                           
+#SBATCH --no-requeue
+#SBATCH --partition=alpha
+#SBATCH --nodes=1                   
+#SBATCH --gres=gpu:1
+#SBATCH --mem=8000
+#SBATCH --mincpus=1
+#SBATCH --time=6-23:59:59                            
+#SBATCH --job-name=baal
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=serhij16@live.de
+#SBATCH --output=output-jobId-%j.out
+#SBATCH --error=error-jobId-%j.out
+
+module --force purge                          				
+module load modenv/hiera CUDA/11.7.0 GCCcore/11.3.0 Python/3.10.4
+
+source lib_var.sh
+
+#remove_new_environment pyjob_baal
+create_or_reuse_environment
+
+# experiments x3 for every run
+echo "Experiments"
+cd /home/sebo742e/baal-serhiy/experiments
+
+echo "Start"
+# cifar10 original data
+#python vgg_mcdropout_cifar10_original.py --epoch 50 --initial_pool 100 --learning_epoch 5
+#python vgg_mcdropout_cifar10_original.py --epoch 50 --initial_pool 100 --learning_epoch 5 --heuristic "entropy"
+
+# cifar10 original + augmented data
+#python vgg_mcdropout_cifar10_org+aug.py --epoch 50 --initial_pool 100 --learning_epoch 5
+#python vgg_mcdropout_cifar10_org+aug.py --epoch 50 --initial_pool 100 --learning_epoch 5 --heuristic "entropy"
+
+# mittelwert (nicht trainierte model!)
+#python vgg_mcdropout_cifar10_org+aug_mittelwert.py --epoch 50 --initial_pool 100 --learning_epoch 5
+#python vgg_mcdropout_cifar10_org+aug_mittelwert.py --epoch 50 --initial_pool 100 --learning_epoch 5 --heuristic "entropy"
+
+# Standardabweichung (nicht trainierte model!)
+#python vgg_mcdropout_cifar10_org+aug_standartabweichung.py --epoch 50 --initial_pool 100 --learning_epoch 5
+#python vgg_mcdropout_cifar10_org+aug_standartabweichung.py --epoch 50 --initial_pool 100 --learning_epoch 5 --heuristic "entropy"
+
+# Varianz (nicht trainierte model!)
+python vgg_mcdropout_cifar10_org+aug_varianz.py --epoch 50 --initial_pool 100 --learning_epoch 5
+#python vgg_mcdropout_cifar10_org+aug_varianz.py --epoch 50 --initial_pool 100 --learning_epoch 5 --heuristic "entropy"
+
+
+echo "End"
